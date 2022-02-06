f170550860f06a0cae572575d8bbefcbd5bba97e
Elif Yönel <41699132+ElifYonel@users.noreply.github.com>
2022-02-06 21:03:32 +0300

    Update jointransactions.sql

--- jointransactions.sql
+++ jointransactions.sql
@@ -1,3 +1,21 @@
 --JOIN
 USE supermarket;
+
+-- 1.INNER JOIN
 SELECT companyId, companyName,material,market FROM company INNER JOIN information ON company.companyName=information.brand;
+
+-- 2.LEFT JOIN
+SELECT * FROM company LEFT JOIN information ON company.companyName=information.brand;
+SELECT companyId, companyName,material,market FROM company LEFT JOIN information 
+                                              ON company.companyName=information.brand 
+                                              WHERE company.companyId < information.id 
+                                              ORDER BY company.companyId;
+
+-- 3.RIGHT JOIN
+SELECT companyId, companyName,material,market FROM company RIGHT JOIN information ON company.companyName=information.brand ORDER BY companyName DESC;
+
+-- 4.FULL JOIN
+SELECT companyName,material,market FROM company FULL JOIN information ON company.companyName=information.brand ORDER BY material ASC;
+
+-- 5.CROSS JOIN
+SELECT * FROM company,information;
